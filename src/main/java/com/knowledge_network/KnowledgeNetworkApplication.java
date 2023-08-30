633b4d6b78be8db65c59b85e7ed3b82b0dd350ee
Buzgau Catalin <buzgaucatalin@yahoo.com>
2023-08-30 17:26:09 +0300

    First commit

---
+++ src/main/java/com/knowledge_network/KnowledgeNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.knowledge_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knowledge_network/KnowledgeNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledge_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
